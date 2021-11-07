c7bdd85335ad14d52383a4d26453059405457bf8
Aleksandr Barmin <barmin.alexander@gmail.com>
2021-11-07 10:27:49 +0000

    Removing AsciiDoctor - doesn't work

--- aws/99_slides/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bundle config --local github.https true
-bundle --path=.bundle/gems --binstubs=.bundle/.bin
-git clone -b 3.9.2 --depth 1 https://github.com/hakimel/reveal.js.git
--- aws/99_slides/render.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bundle exec asciidoctor-revealjs -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2 slides.adoc
